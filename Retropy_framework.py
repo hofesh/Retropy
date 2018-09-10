0324c71c88348533063ebbac5c94029d1fd56c62
hofesh <>
2018-09-10 12:15:32 +0300

    cleanup

--- Retropy_framework.py
+++ Retropy_framework.py
@@ -4616,3 +4616,24 @@ def show_dd_price_actions(target, base, min_days=10, min_depth=1, dd_func=dd_rol
     print("mutual_dd_risk: ", mutual_dd(target, base, dd_func=dd_func))
 
 #################################
+
+from pathlib import Path, PosixPath
+def iter_cached_symbols(skip_fails=True):
+    path = get_symbols_path()
+    for file in Path(path).glob('**/*'):
+        if not file.is_file():
+            continue
+        if str(file).endswith("_FAIL_") and skip_fails:
+            continue
+        symname = file.name.replace("._FAIL_", "").replace(".gz", "")
+        source = file.parts[-2]
+        yield (symname, source)
+
+def get_all_cached_symbols():
+    for sym, source in iter_cached_symbols():
+        get(sym, source=source, error='ignore')
+
+#list(iter_cached_symbols())
+#get_all_cached_symbols()
+
+#############################
--- tmp.py
+++ tmp.py
@@ -1,21 +1,2 @@
 from Retropy_framework import *
 
-
-from pathlib import Path, PosixPath
-def iter_cached_symbols(skip_fails=True):
-    path = get_symbols_path()
-    for file in Path(path).glob('**/*'):
-        if not file.is_file():
-            continue
-        if str(file).endswith("_FAIL_") and skip_fails:
-            continue
-        symname = file.name.replace("._FAIL_", "").replace(".gz", "")
-        source = file.parts[-2]
-        yield (symname, source)
-
-def get_all_cached_symbols():
-    for sym, source in iter_cached_symbols():
-        get(sym, source=source, error='ignore')
-
-#list(iter_cached_symbols())
-get_all_cached_symbols()
